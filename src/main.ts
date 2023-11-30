5cd08b08ec3d55bb035c56478899039b45542bd3
alanllamas <alanllamasg@gmail.com>
2023-11-29 18:55:48 -0600

    feat: used bodyparser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import type { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { rawBody: true })
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { rawBody: true })
+  app.useBodyParser('text');
   await app.listen(3000);
 }
 bootstrap();
