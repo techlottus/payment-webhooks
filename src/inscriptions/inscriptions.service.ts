b64f0a3f25cef82723b524ed3279d4e08b24444b
alan llamas <alanllamasg@gmail.com>
2023-11-30 13:32:59 -0600

    chore: added definitions to path

--- src/inscriptions/inscriptions.service.ts
+++ src/inscriptions/inscriptions.service.ts
@@ -5,7 +5,7 @@ export class InscriptionsService {
   populateStrapi(request: any, response: any) {
     const formResponse = request.body.form_response
     console.log('formResponse: ', formResponse);
-    const answers = formResponse.fields.map((field, index) => {
+    const answers = formResponse.definition.fields.map((field, index) => {
       const answerField =  { ...field, answer: formResponse.answers[index]}
       console.log('answerField: ', answerField);
       
