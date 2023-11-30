86a220599e0751c7957876e94b6ad6f4bf95f683
alan llamas <alanllamasg@gmail.com>
2023-11-30 14:28:16 -0600

    feat: catched cs_id from typeform, return error if not present

--- src/inscriptions/inscriptions.service.ts
+++ src/inscriptions/inscriptions.service.ts
@@ -1,19 +1,28 @@
 import { Body, Injectable, Post, Req } from '@nestjs/common';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class InscriptionsService {
   populateStrapi(request: any, response: any) {
     const formResponse = request.body.form_response
     console.log('formResponse: ', formResponse);
-    const answers = formResponse.definition.fields.map((field, index) => {
-      const { title, type, id } = field
-      const answer = formResponse.answers[index][formResponse.answers[index].type]
-      const answerField =  { id, title, type, answer: type === "multiple_choice" ? answer.label : answer }
-      console.log('answerField: ', answerField);
-      
-      return answerField
-    })
+    const cs_id = formResponse.hidden?.checkout_session_id || null
+
+    if (!!cs_id) {
+      response.status(400).send(`Webhook Error: Not checkout session id has been provided`);
+  
+    } else {
+      const answers = formResponse.definition.fields.map((field, index) => {
+        const { title, type, id } = field
+        const rawAnswer = formResponse.answers[index]
+        const answer = rawAnswer[rawAnswer.type]
+        const answerField =  { id, title, type, answer: type === "multiple_choice" ? answer.label : answer }
+        
+        return answerField
+      })
+    }
     // console.log('answers: ', answers);
+    response.send();
     
   }
 }
