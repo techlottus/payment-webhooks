ec1d28541dda8257e7e92e17d643d8ccfc418b67
alan llamas <alanllamasg@gmail.com>
2023-11-30 18:33:15 -0600

    chore: removed nuber from repeated fields

--- src/inscriptions/inscriptions.service.ts
+++ src/inscriptions/inscriptions.service.ts
@@ -4,6 +4,7 @@ export class InscriptionsService {
   populateStrapi(request: any, response: any) {
     const formResponse = request.body.form_response
     const cs_id = formResponse.hidden.checkout_session_id || null
+    const repeatedFields = ['RFC', 'CFDI_use', 'tax_regime']
 
     if (!cs_id) {
       response.status(400).send(`Webhook Error: Not checkout session id has been provided`);
@@ -20,8 +21,11 @@ export class InscriptionsService {
           const strapiField = { [ref]: type === "multiple_choice" ? answer.label : answer }
           acc.inscription = { ...acc.inscription, ...strapiField }
         } else if (index > acc.needInvoiceIndex) {
+
           const [ _first, ...rest ] = ref.split('_')
-          const key = rest.join('_')
+          const hasRepeatedField = repeatedFields.map((rf) => (ref as string).includes(rf))
+          const key = hasRepeatedField.includes(true) ? rest.pop().join('_') : rest.join('_');
+
           const strapiField = { [key]: type === "multiple_choice" ? answer.label : answer }
           acc.invoice = { ...acc.invoice, ...strapiField }
         }
