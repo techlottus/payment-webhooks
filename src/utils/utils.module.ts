72d4fb5d3bd35efa72aa382c78e1a54790fe9d0a
alan llamas <alanllamasg@gmail.com>
2023-12-04 15:00:33 -0600

    chore: changed service for module

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UtilsService } from './utils.service';
-import { HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports:[HttpService],
+  imports:[HttpModule],
   providers: [UtilsService]
 })
 export class UtilsModule {}
