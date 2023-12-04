caacaa4a48a81b81c48e1ee354a82d0c9140c79e
alan llamas <alanllamasg@gmail.com>
2023-12-04 15:09:48 -0600

    chore: removed providers

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UtilsService } from './utils.service';
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports:[HttpModule],
-  providers: [UtilsService, HttpService]
+  imports:[HttpModule]
 })
 export class UtilsModule {}
