824dd07f6aa28316d7f7ef69f744031b8bc3b5fb
alan llamas <alanllamasg@gmail.com>
2023-12-04 14:34:10 -0600

    chore: moved post strpai to app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-
+require('dotenv').config();
+import { env } from 'process';
 @Injectable()
 export class AppService {
+  constructor(private readonly http: HttpService) {}
+  
   getHello(): string {
     return 'Hello World!';
+  }
+  async postStrapi (endpoint: string, data: any) {
+    return this.http.post(`${env.STRAPI_TRACKING_API}/${endpoint}`, { data }, { headers: { Authorization: `Bearer ${env.STRAPI_TRACKING_TOKEN}` }})
   }
 }
