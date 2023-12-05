5cbaa14e48824eb6230cd0dfa7bd9ec63b1e2966
alan llamas <alanllamasg@gmail.com>
2023-12-04 18:53:08 -0600

    chore: added async aeait to webhook method

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -5,11 +5,10 @@ import { StripeService } from './stripe.service';
 export class StripeController {
   constructor(private readonly stripeService: StripeService) {}
   @Post('/new')
-  webhook(@Req() request: RawBodyRequest<Request>, @Res() response: any ) {
+  async webhook(@Req() request: RawBodyRequest<Request>, @Res() response: any ) {
     // console.log("request: ", request);
     try {
-      
-      this.stripeService.populateStrapi(request, response)
+      await this.stripeService.populateStrapi(request, response)
     } catch (error) {
       console.error(error.message)
     }
