9e82cf5843d0c03e4813a1bb2feed930d658c3dc
alan llamas <alanllamasg@gmail.com>
2023-12-05 12:33:48 -0600

    chore: removed log

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -8,10 +8,7 @@ export class UtilsService {
   constructor(private readonly http: HttpService) {}
 
   postStrapi (endpoint: string, data: any) {
-    const request = { data }
-    console.log(request);
-    
-    return this.http.post(`${env.STRAPI_TRACKING_API}/${endpoint}`, request, { headers: { Authorization: `Bearer ${env.STRAPI_TRACKING_TOKEN}` }})
+    return this.http.post(`${env.STRAPI_TRACKING_API}/${endpoint}`, { data }, { headers: { Authorization: `Bearer ${env.STRAPI_TRACKING_TOKEN}` }})
   }
 
 }
