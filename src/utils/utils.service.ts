dc107661a0c489ffb47008bea3606c34dc5d56bf
alan llamas <alanllamasg@gmail.com>
2023-12-06 16:05:56 -0600

    chore: added fetchstrapi method

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -6,9 +6,13 @@ import { env } from 'process';
 @Injectable()
 export class UtilsService {
   constructor(private readonly http: HttpService) {}
+  config = { headers: { Authorization: `Bearer ${ env.STRAPI_TRACKING_TOKEN }` } }
 
   postStrapi (endpoint: string, data: any) {
-    return this.http.post(`${env.STRAPI_TRACKING_API}/${endpoint}`, { data }, { headers: { Authorization: `Bearer ${env.STRAPI_TRACKING_TOKEN}` }})
+    return this.http.post(`${env.STRAPI_TRACKING_API}/${endpoint}`, { data }, this.config)
+  }
+  fetchStrapi = async (model: string, params: string[] ) => {
+    return this.http.get(`${env.STRAPI_TRACKING_API}/${model}${!!params.length && '?' + params.join('&')}`, this.config)
   }
 
 }
