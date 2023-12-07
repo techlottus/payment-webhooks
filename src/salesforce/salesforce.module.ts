08124a9a80c90792d126a360800aa835f70ba74e
alan llamas <alanllamasg@gmail.com>
2023-12-06 18:03:25 -0600

    chore: added utils module to salesforce

--- src/salesforce/salesforce.module.ts
+++ src/salesforce/salesforce.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SalesforceService } from './salesforce.service';
 import { SalesforceController } from './salesforce.controller';
+import { UtilsModule } from 'src/utils/utils.module';
 
 @Module({
+  imports: [UtilsModule],
   providers: [SalesforceService],
   controllers: [SalesforceController]
 })
