53cc7a57c103bd7b3d00793ff1608c4e2be66090
alan llamas <alanllamasg@gmail.com>
2023-12-12 19:10:53 -0600

    chore: removed call

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -21,7 +21,6 @@ export class UtilsService {
     return this.http.get(`${env.SF_OFFER_ENDPOINT}?linea=${brand}&campus=${campus}`, { headers: { Authorization: `${token_type} ${token}` }})
   }
   authSF(){
-    // return this.http.post(`https://lottus.my.salesforce.com/services/oauth2/token?client_id=3MVG9lIH5nSM6mjR1sxnwiuAneeDhWnxWVLzYW81MeLtMPkzBZF8DhWnsEa9510OrZzhCtfv7g0ENsn9EswT1&client_secret=EF01E2534DFE69292B4F2EE195A6EECE681020DF19DA66AF967C9CECEBF375E5&username=utc_integracion@lottuseducation.com&password=CRMLottusUTC2022*goQ3EioytWAEJl1HL9Lo23xzK&grant_type=password`)
     return this.http.post(`${env.SF_AUTH_ENDPOINT}?client_id=${env.SF_CLIENT_ID}&client_secret=${env.SF_CLIENT_SECRET}&username=${env.SF_USERNAME}&password=${env.SF_PASSWORD}&grant_type=${env.SF_GRANT_TYPE}`)
   }
 }
