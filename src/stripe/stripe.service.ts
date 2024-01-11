5ecd960509eac10402c8fb13d69c9192e13b9e2c
alan llamas <alanllamasg@gmail.com>
2024-01-11 15:52:54 -0600

    changed destructuring for assignment and added logs

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -59,19 +59,7 @@ export class StripeService {
 
   async checkoutSessionCompleted(event: any) {
     const checkout_session_id = event.data.object.id
-    const {
-      id: cs_id,
-      line_items,
-      payment_intent: payment_intent_id,
-      payment_status: status,
-      amount_total,
-      customer_details: { email, phone },
-      metadata,
-      customer: { id: customer_id},
-      payment_method_types,
-      subscription,
-      payment_link: { after_completion, after_completion: { type } }
-    } = await stripe.checkout.sessions.retrieve(
+    const checkoutSessionCompleted = await stripe.checkout.sessions.retrieve(
       checkout_session_id,
       {
         expand: [
@@ -85,25 +73,49 @@ export class StripeService {
         ],
       }
     );
+    console.log('checkoutSessionCompleted: ', checkoutSessionCompleted);
+    
+    
+    const cs_id = checkoutSessionCompleted.id
     console.log('cs_id: ', cs_id);
     
-    // const {
-    //   id: cs_id,
-    //   line_items,
-    //   payment_status: status,
-    //   amount_total,
-    //   customer_details: { email, phone },
-    //   metadata,
-    //   customer: { id: customer_id},
-    //   payment_method_types,
-    //   payment_link: { after_completion, after_completion: { type } }
-    // } = checkoutSessionCompleted
-
+    const line_items = checkoutSessionCompleted.line_items
+    console.log('line_items: ', line_items);
     
-
-    console.log('checkoutSessionCompleted.payment_intent: ', payment_intent_id);
-    const payment_id = !!subscription ? subscription?.latest_invoice?.charge?.payment_intent : payment_intent_id
-    const subscription_id = !!subscription ? subscription?.latest_invoice?.charge?.id : null
+    const status = checkoutSessionCompleted.payment_status
+    console.log('status: ', status);
+    
+    const amount_total = checkoutSessionCompleted.amount_total
+    console.log('amount_total: ', amount_total);
+    
+    const email = checkoutSessionCompleted.customer_details.email
+    console.log('email: ', email);
+    
+    const phone = checkoutSessionCompleted.customer_details.phone
+    console.log('phone: ', phone);
+    
+    const metadata = checkoutSessionCompleted.metadata
+    console.log('metadata: ', metadata);
+    
+    const customer_id = checkoutSessionCompleted.customer.id
+    console.log('customer_id: ', customer_id);
+    
+    const payment_method_types = checkoutSessionCompleted.payment_method_types
+    console.log('payment_method_types: ', payment_method_types);
+    
+    const after_completion = checkoutSessionCompleted.payment_link.after_completion
+    console.log('after_completion: ', after_completion);
+    
+    const type = after_completion.type
+    console.log('type: ', type);
+    
+    console.log('checkoutSessionCompleted.payment_intent: ', checkoutSessionCompleted.payment_intent);
+    
+    const payment_id = !!checkoutSessionCompleted.subscription ? checkoutSessionCompleted?.subscription?.latest_invoice?.charge?.payment_intent : checkoutSessionCompleted.payment_intent
+    console.log('payment_id: ', payment_id);
+    
+    const subscription_id = !!checkoutSessionCompleted.subscription ? checkoutSessionCompleted?.subscription?.latest_invoice?.charge?.id : null
+    console.log('subscription_id: ', subscription_id);
 
     console.log('payment_id: ', payment_id);
     const typeform_url = after_completion[type].url.replace('{CHECKOUT_SESSION_ID}', cs_id)
@@ -114,7 +126,7 @@ export class StripeService {
       ]
     })
     console.log('payment_intent: ', payment_intent);
-    const order_id = !!subscription ? subscription?.latest_invoice?.charge?.id : payment_intent.latest_charge
+    const order_id = checkoutSessionCompleted.subscription ? checkoutSessionCompleted?.subscription?.latest_invoice?.charge?.id : payment_intent.latest_charge
     console.log('order_id: ', order_id);
 
     const request = {
