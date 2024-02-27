ed5a80071cfe5fbb2646e2f0dc465d4201a8e548
alan llamas <alanllamasg@gmail.com>
2024-02-27 15:12:36 -0600

    feat: validated curp on inscription data

--- src/inscriptions/inscriptions.service.ts
+++ src/inscriptions/inscriptions.service.ts
@@ -1,5 +1,5 @@
 import {  Injectable } from '@nestjs/common';
-import { catchError, forkJoin, of } from 'rxjs';
+import { catchError, forkJoin, mergeMap, of } from 'rxjs';
 import { UtilsService } from 'src/utils/utils.service';
 
 @Injectable()
@@ -42,27 +42,62 @@ export class InscriptionsService {
         }
         return acc
       }, { inscription: { cs_id, submitted_at }, invoice: { cs_id, submitted_at }, needInvoiceIndex: null, needInvoice: false })
+      const curp = answers.inscription.CURP.toUpperCase() || null
 
       try {
-        const inscriptionObs = this.utilsService.postStrapi('track-inscriptions', answers.inscription)
-        const invoiceObs = this.utilsService.postStrapi('track-invoices', answers.invoice)
+        this.utilsService.postSelfWebhook('/curp/validate', {curp})
+        .pipe(
+          catchError((err, caught) => {
+            // console.log(err);
+            
+            response.status(err.response.status).send(err.response.data);
+            return of({ error: true, ...err})
+          }),
+          mergeMap((res) => {
+            if (res.error) {
+              return of(res)
+            }
+            const inscription = {
+              ...answers.inscription,
+              name: res.data.nombre,
+              CURP: res.data.curp,
+              last_name: res.data.apellidoPaterno,
+              second_last_name: res.data.apellidoMaterno,
+              gender: res.data.sexo,
+              birthdate: res.data.fechaNacimiento,
+              residence: 'Nacional',
+              birth_entity: res.data.estadoNacimiento
+            }
+            // console.log(inscription);
+            
+            const inscriptionObs = this.utilsService.postStrapi('track-inscriptions', inscription)
+            const invoiceObs = this.utilsService.postStrapi('track-invoices', answers.invoice)
+    
+            const sources = [ inscriptionObs ]
+            if (answers.needInvoice) sources.push(invoiceObs)
 
-        const sources = [ inscriptionObs ]
-        if (answers.needInvoice) sources.push(invoiceObs)
+            return forkJoin(sources).pipe(catchError((err) => {
+              // console.log(err)
+              // response.status(err.response.status).send(err.response.data);
 
-        forkJoin(sources).pipe(catchError((err) => {
-          console.log(err)
-          return of(err)
-        })).subscribe(data => {
-          response.status(data[0].status)
-          this.utilsService.callSelfWebhook('/salesforce/inscription', { cs_id }).subscribe()
-          // guardar need invoice data
-        })
+              return of({ error: true, ...err})
+            }))
+          }),
+          mergeMap(res => {
+            if (res.error) {
+              return of(res)
+            }
+            return this.utilsService.postSelfWebhook('/salesforce/inscription', { cs_id })
+          })
+        )
+       .subscribe(() => {
+
+         response.send()
+       })
         
       } catch (error) {
-        response.status(error.status).send(error.message);
+        response.status(error.status).send(error.message || error.data);
       }
-      response.send()
     }
   }
 }
