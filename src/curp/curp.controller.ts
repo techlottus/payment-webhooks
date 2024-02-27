6b651c88694978cf84ef758ff6921f265da41ea9
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-02-27 13:41:32 -0600

    post curp using validate and fetch

--- src/curp/curp.controller.ts
+++ src/curp/curp.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
+import { CurpService } from './curp.service';
+import { catchError } from 'rxjs';
 
 @Controller('curp')
-export class CurpController {}
+export class CurpController {
+    constructor(private readonly curpService: CurpService) { }
+
+    @Post('/validate')
+    webhook(@Req() request: any, @Res() response: any) {
+        const ParamsHasError = !request.body.curp
+        const ParamsError = `Missing parameters: ${request.body.curp ? '' : 'curp, please check call.'}`
+        if (ParamsHasError) return response.status(400).send(ParamsError)
+        const curp = this.curpService.validateCURP(request.body.curp)
+        if (curp) {
+            this.curpService.fetchCURP(curp).pipe(
+                catchError((err, caught) => {
+                    response.status(err.response.data.status).send(err.response.data.message)
+                    return caught
+                })
+            ).subscribe(res => {
+                return response.status(200).send({ ...res.data })
+            })
+        } else {
+            return response.status(400).send("CURP is invalid, please check params")
+        }
+    }
+}
