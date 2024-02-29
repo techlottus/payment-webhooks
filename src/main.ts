6730dc009d3268b46369aa2e7596bfe23a546f82
alan llamas <alanllamasg@gmail.com>
2024-02-29 17:04:59 -0600

    chore: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { rawBody: true })
   app.useBodyParser('text');
+  app.enableCors();
   await app.listen(process.env.HOST_PORT);
 }
 bootstrap();
