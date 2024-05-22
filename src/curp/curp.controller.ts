e17edf75b326d8ca9240cd60d14c194d3c0825a4
alan llamas <alanllamasg@gmail.com>
2024-05-22 14:51:51 -0500

    chore: removed validate curp

--- src/curp/curp.controller.ts
+++ src/curp/curp.controller.ts
@@ -14,25 +14,25 @@ export class CurpController {
         const ParamsError = `Missing parameters: ${request.body.curp ? '' : 'curp, please check call.'}`
         if (ParamsHasError) return response.status(400).send(ParamsError)
         console.log('request.body.curp: ', request.body.curp);
-        const curp = this.curpService.validateCURP(request.body.curp)
-        if (curp) {
-            this.curpService.fetchCURP(curp).pipe(
-                catchError((err, caught) => {
-                    console.log('err.response: ', err.response);
-                    console.log('err.response.data: ', err.response.data);
-                    
-                    return of({error: true, ...err})
-                })
-            ).subscribe(res => {
-                if (res.error) {
-                    return response.status(res.response.data.status).send(res.response.data.message)
-                }
-                if (res.data) {
-                    return response.status(200).send({ ...res.data })
-                }
+        // const curp = this.curpService.validateCURP(request.body.curp)
+        // if (curp) {
+        this.curpService.fetchCURP(request.body.curp).pipe(
+            catchError((err, caught) => {
+                console.log('err.response: ', err.response);
+                console.log('err.response.data: ', err.response.data);
+                
+                return of({error: true, ...err})
             })
-        } else {
-            return response.status(400).send("CURP is invalid, please check params")
-        }
+        ).subscribe(res => {
+            if (res.error) {
+                return response.status(res.response.data.status).send(res.response.data.message)
+            }
+            if (res.data) {
+                return response.status(200).send({ ...res.data })
+            }
+        })
+        // } else {
+        //     return response.status(400).send("CURP is invalid, please check params")
+        // }
     }
 }
