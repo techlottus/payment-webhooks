e2b1540ecceb179f40fa8ede08e4952cf4702834
Yafté M. <redyafte@gmail.com>
2024-05-28 20:00:59 -0600

    feat: added utils module and service to flywire moldule

--- src/flywire/flywire.module.ts
+++ src/flywire/flywire.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FlywireService } from './flywire.service';
 import { FlywireController } from './flywire.controller';
+import { UtilsService } from 'src/utils/utils.service';
+import { UtilsModule } from 'src/utils/utils.module';
 
 @Module({
-  providers: [FlywireService],
-  controllers: [FlywireController]
+  imports: [UtilsModule],
+  providers: [FlywireService, UtilsService],
+  controllers: [FlywireController],
 })
 export class FlywireModule {}
