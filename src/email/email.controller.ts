dd826d8d82679d665062384a6d0449d4838c6024
alan llamas <alanllamasg@gmail.com>
2024-09-24 18:30:08 -0500

    feat: added send trackiing data after sending mail

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -3,18 +3,35 @@ import { UtilsService } from 'src/utils/utils.service';
 import { EmailService } from './email.service';
 import { catchError, mergeMap, of, take } from 'rxjs';
 import Handlebars from "handlebars";
+import { parseString } from "xml2js";
+
+type SendEmailBody = {
+  template: string;
+  subject: string;
+  toAddress: string;
+  priority?: string;
+  ccToAddress: string;
+  template_id?: string;
+  params?: any;
+  scope?: string;
+  template_name?: string;
+}
 
 @Controller('email')
 export class EmailController {
   constructor(public utils: UtilsService, public email: EmailService) {}
 
   @Post('/salesforce/send')
-  sendEmail(@Body() body: {template: string, subject: string, toAddress: string, priority?: string, ccToAddress: string }, @Res() response: any ) {
+  sendEmail(@Body() body: SendEmailBody, @Res() response: any ) {
     // console.log(body);
     const {
       template, 
       subject, 
       toAddress,
+      template_id,
+      params,
+      scope,
+      template_name
     } = body
     
     const priority = body.priority || 'Normal'
@@ -23,12 +40,63 @@ export class EmailController {
       mergeMap(authRes => {
         const token = authRes.data.access_token
         const xml = this.email.generateXML(token, template, subject, toAddress, priority, ccToAddress )
-        
-       return  this.utils.sendSFemail(xml)
+
+        return  this.utils.sendSFemail(xml)
       }),
-      catchError((err, caught)=> { console.log(err); return caught })
+      catchError((err, caught)=> { console.log(err); return caught }),
+      mergeMap(sendRes => {
+        console.log('sendRes: ', sendRes.data);
+        let Res;
+        parseString(sendRes.data, (err, data) => {
+          if (err) {
+            console.log('err: ', err);
+            Res = err
+          }
+          if (data) {
+            const headersDigestedData = data['soapenv:Envelope']['soapenv:Header']
+            const bodyDigestedData = data['soapenv:Envelope']['soapenv:Body'][0].sendEmailResponse[0].result[0]
+            const { errors, success } = bodyDigestedData
+            
+            console.log('headersDigestedData: ', headersDigestedData);
+            console.log('errors: ', errors);
+            console.log('success: ', success);
+            Res = { headers: headersDigestedData, ...bodyDigestedData}
+
+          }
+        })
+
+        const { current, limit } = Res.headers[0].LimitInfoHeader[0].limitInfo[0]
+
+        const errors = Res.errors?.map(error => {
+          const { fields, message, statusCode } = error
+          const newfields = fields.map( field => { return {field} });
+          console.log(newfields);
+          return {
+            fields: newfields,
+            message: message[0],
+            statusCode: statusCode[0],
+          }
+        })
+        // console.log(errors);
+        return this.utils.postStrapi('track-send-emails', {
+          template: template_name,
+          template_id,
+          params,
+          scope,
+          compiled_template: template,
+          email: toAddress,
+          subject,
+          delivered: Res.success[0] === 'true',
+          daily_current_count: current[0],
+          sending_limit: limit,
+          errors
+        })
+      })
     ).subscribe(res => {
-      response.send(res.data)
+      console.log(res);
+      
+        response.send()
+      
     })
   }
 
@@ -50,7 +118,7 @@ export class EmailController {
       const compiled = template(params)
       // console.log(compiled);
       
-      response.send(JSON.stringify({ compiled, params, template: template_data}))
+      response.send(JSON.stringify({ compiled, params, template: template_data, template_id: res.data.data.id}))
     } )
     // body.data
   }
