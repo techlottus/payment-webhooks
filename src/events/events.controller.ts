c450db901f2138239437e0112ca97b82cdfb7b55
alan llamas <alanllamasg@gmail.com>
2025-02-13 14:18:52 -0600

    chore: filtered event model

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Post, Req, Res, Response } from '@nestjs/common';
 
 @Controller('events')
 export class EventsController {
   @Post('/created')
-  async created(@Req() req) {
-    console.log(req.body);
-    
-    return 'created';
+  async created(@Req() req, @Res() response: any) {
+    if (req.body.model === 'event') {
+      console.log(req.body);
+
+      response.status(200).send('created');
+      return 'created';
+    } else {
+      response.status(200).send();
+    }
   }
 }
